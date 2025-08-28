62a64996b02f4463ceb87a546cfefecd5090f0b0
Nic2405 <laarnienicoleprajes@gmail.com>
2025-08-28 10:32:15 +0800

    color for buttons

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#02301fff',
     color: 'white',
     borderRadius: 8,
   }
