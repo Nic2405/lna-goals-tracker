626bcf46b2a995e6d4dc5aa847034d9f1198f4b3
Nic2405 <laarnienicoleprajes@gmail.com>
2025-08-28 11:44:15 +0800

    firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_5xq2hYmyFpwlx6kWaFEC7jvpJcH4WTw",
+  authDomain: "lna-goal-tracker.firebaseapp.com",
+  projectId: "lna-goal-tracker",
+  storageBucket: "lna-goal-tracker.firebasestorage.app",
+  messagingSenderId: "763663207864",
+  appId: "1:763663207864:web:1e702c8c641a037711134d"
+};
+
+
+const app = initializeApp(firebaseConfig);
